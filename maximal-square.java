cdac71cecbb86c38115a06dd62cda9c7f1507976
Learner <vivek.gurukul1982@gmail.com>
2020-05-21 12:48:38 -0700

    Create maximal-square.java

---
+++ maximal-square.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/maximal-square/
+
+class maximal-square {
+    // tc -> m*n, sc-> m*n
+    public int maximalSquare(char[][] matrix) {
+        if(matrix==null || matrix.length==0) return 0;
+        int m = matrix.length, n = matrix[0].length;
+        
+        int[][] sum = new int[m+1][n+1];
+        int max = 0;
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(matrix[i-1][j-1]=='1'){
+                    sum[i][j] = Math.min(sum[i-1][j-1], 
+                                  Math.min(sum[i-1][j], sum[i][j-1]))+1;
+                    max = Math.max(max, sum[i][j]);
+                }
+            }
+        }
+        return max*max;
+    }
+}
